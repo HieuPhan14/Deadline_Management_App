63edb4dc797451b92806795bb1ef0120d8434118
Trung Hieu <phantrunghieu.lnh@gmail.com>
2026-05-04 21:46:19 -0700

    add StaffDetail page

--- deadline-app/frontend/src/pages/StaffDetail.jsx
+++ deadline-app/frontend/src/pages/StaffDetail.jsx
@@ -1,3 +1,43 @@
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { getStaffTasks } from '../api/staff'
+import TaskCard from '../components/TaskCard'
+
 export default function StaffDetail() {
-    return <div>Staff Detail</div>
+    const [tasks, setTasks] = useState([])
+    const [loading, setloading] = useState(true)
+    const { staffId } = useParams()
+    const navigate = useNavigate()   
+    
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const data = await getStaffTasks(staffId)
+                setTasks(data)
+            } catch {
+                console.log("Can't get data")
+            } finally {
+                setloading(false)
+            }
+        }
+        fetchTasks()
+    }, [staffId])
+
+    if (loading) return <div>Loading...</div>
+
+    return (
+        <div>
+            <button onClick={() => navigate('/dashboard')}>
+                Back
+            </button>
+            <h1>Staff Tasks</h1>
+            {tasks.length === 0
+                ? <p>No task assigned</p>
+                : tasks.map(task => (
+                    <TaskCard key={task.id} task={task} />
+                ))
+
+            }
+        </div>
+    )
 }
